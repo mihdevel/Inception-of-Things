aba24c779dc9e9bdc1faf3d6d4d487bc33badd15
Magikarp Eunostus <meunostu@oa-j1.msk.21-school.ru>
2022-08-16 21:46:50 +0300

    v1.1

--- p1/provision.sh
+++ p1/provision.sh
@@ -1,9 +1,12 @@
-yum update
-sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+
+yum update -y
+sudo cat > /etc/yum.repos.d/kubernetes.repo << EOF
 [kubernetes]
 name=Kubernetes
 baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
+enabled=1   
 gpgcheck=1
 repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
