0cd12cc4796aa7206b18413f2c4bba7f572553c5
meunostu <mihdevel@gmail.com>
2022-08-22 19:54:23 +0300

    P1-v2

--- p1/provision.sh
+++ p1/provision.sh
@@ -2,15 +2,10 @@ sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /
 sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 
 yum update -y
-sudo cat > /etc/yum.repos.d/kubernetes.repo << EOF
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1   
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
-sudo yum install -y kubectl
-kubectl version --client
-kubectl cluster-info
+
+echo "Установка k3s"
+curl -sfL https://get.k3s.io | sh -
+chmod 644 /etc/rancher/k3s/k3s.yaml
+
+echo "Проверка сервиса k3s"
+service k3s.service status
