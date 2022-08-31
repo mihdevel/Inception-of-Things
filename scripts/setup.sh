1954c89120414478d58590f01704e381f77bca63
meunostu <mihdevel@gmail.com>
2022-08-31 19:37:41 +0300

    31.08.22

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,13 @@
 sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 
-curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--flannel-iface en0" sh -s -
+echo "Установка k3s"
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--bind-address=192.168.42.110 --flannel-iface en0: Ethernet" sh -
+chmod 644 /etc/rancher/k3s/k3s.yaml
+
+echo "Проверка сервиса k3s"
+service k3s.service status
+
 kubectl apply -f /vagrant/confs/app-one.yaml >/dev/null 2>&1
 kubectl apply -f /vagrant/confs/app-two.yaml >/dev/null 2>&1
 kubectl apply -f /vagrant/confs/app-three.yaml >/dev/null 2>&1
