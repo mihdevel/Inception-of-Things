cd3d9ad11af43255ad9fb64a2b6a9931d6cf2625
meunostu <mihdevel@gmail.com>
2022-09-11 20:07:56 +0300

    Рабочая версия, IP Проверить

--- p2/scripts/setup.sh
+++ p2/scripts/setup.sh
@@ -2,14 +2,22 @@
 sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 
+#echo "Установка net-tools"
+#yum -y install net-tools
+
 echo "Установка k3s"
-curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--bind-address=192.168.42.110 --flannel-iface en0: Ethernet" sh -
-chmod 644 /etc/rancher/k3s/k3s.yaml
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--bind-address=192.168.56.110 --node-ip 192.168.56.110 --tls-san 192.168.56.110" sh -
 
-echo "Проверка сервиса k3s"
-service k3s.service status
+echo "Поиск kubectl"
+which kubectl # Показывает где находится исполняемаю программа
 
-kubectl apply -f /vagrant/confs/app-one.yaml >/dev/null 2>&1
-kubectl apply -f /vagrant/confs/app-two.yaml >/dev/null 2>&1
-kubectl apply -f /vagrant/confs/app-three.yaml >/dev/null 2>&1
-kubectl apply -f /vagrant/confs/ingress.yaml >/dev/null 2>&1
+echo "kubectl apply files"
+/usr/local/bin/kubectl apply -f /vagrant/confs/app-one.yaml
+/usr/local/bin/kubectl apply -f /vagrant/confs/app-two.yaml
+/usr/local/bin/kubectl apply -f /vagrant/confs/app-three.yaml
+/usr/local/bin/kubectl apply -f /vagrant/confs/ingress.yaml
+
+echo "Статус кластера"
+/usr/local/bin/kubectl get all
+/usr/local/bin/kubectl get nodes -o wide
+echo "End script"
